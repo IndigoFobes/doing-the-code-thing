dee151f40e0e6a47d8ce5c8ae2b5c450b2d26e79
Indigo <indigo.crandell@gmail.com>
2022-10-06 14:34:05 -0400

    Index file for models to contain associations.

--- Assets/models/Comment.js
+++ Assets/models/Comment.js
@@ -1,37 +1,34 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Blog extends Model {};
+class Comment extends Model {};
 
-Blog.init (
+Comment.init (
     {
-        // blog id
+        // comment id
         id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true,
         },
-        // blog title
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            // **TODO: max characters
-        },
-        // blog contents
-        contents: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            // Any constraints?
-        },
-        // user id
-        author: {
+        // which user posted the comment?
+        user_id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             references: {
                 model: 'user',
                 key: 'id'
-            },
+            }
+        },
+        // reference the blog post under which it was created
+        blog_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'blog',
+                key: 'id'
+            }
         }
     },
     {
@@ -41,8 +38,8 @@ Blog.init (
         updatedAt: false,
         freezeTableName: true,
         underscored: true,
-        modelName: 'blog'
+        modelName: 'comment'
     }
 );
 
-module.exports = Blog
+module.exports = Comment
---
+++ Assets/models/index.js
@@ -0,0 +1,28 @@
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
+
+User.hasMany(Blog, {
+    foreignKey: 'author',
+    // on delete CASCADE? (if I delete a USER, delete their posts)
+});
+
+Blog.belongsTo(User, {
+    foreignKey: 'author',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+});
+
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+});
+
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id',
+});
+
+
+
+module.exports = { User, Blog };
