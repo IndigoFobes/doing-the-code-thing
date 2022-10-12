15997230f21838f91494298705381946b7623f59
Indigo <indigo.crandell@gmail.com>
2022-10-12 13:01:28 -0400

    I now have a get route for comments. Realizing I would have to take out the withAuth helper to test other routes...

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,6 +2,16 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// get all comments
+router.get('/', async (req, res) => {
+    try {
+        const commentData = await Comment.findAll();
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
 // create a comment
 router.post('/', withAuth, async (req, res) => {
     try {
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Comment, Blog } = require('../../models');
 
 
 // get route for all users
 router.get('/', async (req, res) => {
     try {
-        const userData = await User.findAll();
+        const userData = await User.findAll({
+            include: [{ model: Blog }, { model: Comment }]
+        });
         res.status(200).json(userData);
     } catch (err) {
         res.status(400).json(err);
